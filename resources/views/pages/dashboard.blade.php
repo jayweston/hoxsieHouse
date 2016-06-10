2729bd8632959482771ab00985cad6e1c3e75444
Jay Weston <teacum@gmail.com>
2016-06-10 09:40:43 -0600

    footer

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="row">
+		<div class="heading"><a href="http://NiteOwlCreates.HoxsieHouse.com">Nite Owl Creates</a></div>
+		<div class="title"></div>
+	</div>
+	<div class="row">
+		<div class="heading"><a href="http://RedCherries.HoxsieHouse.com">Red Cherries 22</a></div>
+		<div class="title"></div>
+	</div>
+	<div class="row">
+		<div class="heading"><a href="http://StaciesPlace.HoxsieHouse.com">Stacie's Place</a></div>
+		<div class="title"></div>
+	</div>
+	<div class="row">
+		<div class="heading"><a href="http://Travel.HoxsieHouse.com">Tony And Stacie Travel Blog</a></div>
+		<div class="title"></div>
+	</div>
+@endsection
