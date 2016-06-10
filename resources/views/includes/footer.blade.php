45bc18a36e42bfc51ad13a8130e7772cb406f182
Jay Weston <teacum@gmail.com>
2016-06-10 12:24:11 -0600

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,28 @@
 <div class="container" >
-	<div class="row footer_row">Navigation: </div>
-	<div class="row footer_row">Other Sites: </div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="row">
+				<div class="col-xs-6 text-right">
+					<ul class="nav_sites">
+						<li>Navigation</li>
+						<li><a href="{{ url('/post') }}" id="nav_travel" class="nav_site" title="Tony & Stacie Travel">Home</a>&emsp;&emsp;</li>
+						<li><a href="{{ url('/post?type=travel') }}" id="nav_travel" class="nav_site" title="Redcherries">Travel</a>&emsp;&emsp;</li>
+					</ul>				
+				</div>
+				<div class="col-xs-6">
+					<ul class="sister_sites">
+						<li>Sister Sites</li>
+						<li><a target="_blank" href="http://crafts.hoxsiehouse.com/" id="site_craft" class="sister_site" title="Nite Owl Creates">Nite Owl Creates</a></li>
+						<li><a target="_blank" href="http://stacieplace.hoxsiehouse.com/" id="site_stacie" class="sister_site" title="Stacie's Place">Stacie's Place</li>
+						<li><a target="_blank" href="http://travel.hoxsiehouse.com/" id="site_travel" class="sister_site" title="Tony & Stacie Travel">Travel</a></li>
+						<li><a target="_blank" href="http://stacie.hoxsiehouse.com/" id="site_redcherries" class="sister_site" title="Redcherries">Red Cherries</a></li>
+						<li><a target="_blank" href="http://wedding.hoxsiehouse.com/" id="site_wedding" class="sister_site" title="Our Wedding">Wedding</a></li>
+						@if (!Auth::guest()) @if (Auth::user()->type == App\Models\User::TYPE_ADMIN)<li><a target="_blank" href="http://private.hoxsiehouse.com/" id="site_private" class="sister_site" title="Personal Blog">Private</a></li> @endif @endif
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="row footer_row">
 		<ul class="social_links">
 			<li><a target="_blank" href="https://www.facebook.com/TheStaciesPlace" id="social_facebook" class="social_icon" title="Facebook"><img src="/images/icon/social/facebook.png" /></a></li>
@@ -8,7 +30,7 @@
 			<li><a target="_blank" href="https://twitter.com/Stacies_Place" id="social_twitter" class="social_icon" title="Twitter"><img src="/images/icon/social/twitter.png" /></a></li>
 			<li><a target="_blank" href="https://www.pinterest.com/stacies_place/" id="social_pintrest" class="social_icon" title="Pintrest"><img src="/images/icon/social/pinterest.png" /></a></li>
 			<li><a target="_blank" href="https://www.bloglovin.com/people/staciesh-1842418" id="social_bloglovin" class="social_icon" title="BlogLovin"><img src="/images/icon/social/bloglovin.png" /></a></li>
-			<li><a target="_blank" href="snapchat://add/staciesplace" id="social_snapchat" class="social_icon" title="SnapChat'"><img src="/images/icon/social/snapchat.png" /></a></li>
+			<li><a target="_blank" href="snapchat://add/staciesplace" id="social_snapchat" class="social_icon" title="SnapChat"><img src="/images/icon/social/snapchat.png" /></a></li>
 		</ul>
 	</div>
 	<div class="row footer_row">
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 	<div class="row">
-		<div class="heading"><a href="http://NiteOwlCreates.HoxsieHouse.com">Nite Owl Creates</a></div>
-		<div class="title"></div>
-	</div>
-	<div class="row">
-		<div class="heading"><a href="http://RedCherries.HoxsieHouse.com">Red Cherries 22</a></div>
+		<div class="heading"><a href="http://Crafts.HoxsieHouse.com">Nite Owl Creates</a></div>
 		<div class="title"></div>
 	</div>
 	<div class="row">
@@ -14,7 +10,19 @@
 		<div class="title"></div>
 	</div>
 	<div class="row">
+		<div class="heading"><a href="http://Stacie.HoxsieHouse.com">Red Cherries 22</a></div>
+		<div class="title"></div>
+	</div>
+	<div class="row">
 		<div class="heading"><a href="http://Travel.HoxsieHouse.com">Tony And Stacie Travel Blog</a></div>
+		<div class="title"></div>
+	</div>
+	<div class="row">
+		<div class="heading"><a href="http://Wedding.HoxsieHouse.com">Wedding</a></div>
+		<div class="title"></div>
+	</div>
+	<div class="row">
+		<div class="heading"><a href="http://Private.HoxsieHouse.com">Secret</a></div>
 		<div class="title"></div>
 	</div>
 @endsection
