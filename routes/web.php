07b125f7a79e31ee41947fe4f761542e10d2d838
Jay Weston <teacum@gmail.com>
2018-03-12 17:46:56 -0700

    Test locally without logging in by impersonating a user.

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,24 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::auth();
-Route::get('/', 'SinglePageController@dashboard');
-Route::get('/latest', 'SinglePageController@latest');
-Route::get('/unread', 'SinglePageController@unread');
-Route::get('/rss', 'SinglePageController@rss');
-Route::get('/tos', 'SinglePageController@tos');
-Route::get('/privacy', 'SinglePageController@privacy');
-Route::resource('/post', 'PostController');
-Route::resource('/user', 'UserController');
-Route::resource('/tag', 'TagController', ['only' => ['index','show','update','destroy']]);
-Route::resource('/postimage', 'PostImageController', ['only' => ['store','update','destroy']]);
-Route::resource('/postmeta', 'PostMetaController', ['only' => ['store','update','destroy']]);
-Route::resource('/posttag', 'PostTagController', ['only' => ['update']]);
-Route::resource('/comment', 'CommentController', ['only' => ['store','destroy']]);
-Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
-Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+Route::group(['middleware' => 'impersonate'], function () {
+	Route::auth();
+	Route::get('/', 'SinglePageController@dashboard');
+	Route::get('/latest', 'SinglePageController@latest');
+	Route::get('/unread', 'SinglePageController@unread');
+	Route::get('/rss', 'SinglePageController@rss');
+	Route::get('/tos', 'SinglePageController@tos');
+	Route::get('/privacy', 'SinglePageController@privacy');
+	Route::resource('/post', 'PostController');
+	Route::resource('/user', 'UserController');
+	Route::resource('/tag', 'TagController', ['only' => ['index','show','update','destroy']]);
+	Route::resource('/postimage', 'PostImageController', ['only' => ['store','update','destroy']]);
+	Route::resource('/postmeta', 'PostMetaController', ['only' => ['store','update','destroy']]);
+	Route::resource('/posttag', 'PostTagController', ['only' => ['update']]);
+	Route::resource('/comment', 'CommentController', ['only' => ['store','destroy']]);
+	Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
+	Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+});
 Route::get('/test', function () { 
 	$test='http://localhost:8000/post/1#_=_';
 	dd(trim($test, "#_=_"));
