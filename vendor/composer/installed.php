68c9bf932514dc9d626f8c2c1ce3ced45f4354ed
jayweston <teacum@gmail.com>
2021-05-02 14:30:21 -0700

    composer updates

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -577,12 +577,12 @@
     ),
     'nesbot/carbon' => 
     array (
-      'pretty_version' => '2.46.0',
-      'version' => '2.46.0.0',
+      'pretty_version' => '2.47.0',
+      'version' => '2.47.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => '2fd2c4a77d58a4e95234c8a61c5df1f157a91bf4',
+      'reference' => '606262fd8888b75317ba9461825a24fc34001e1e',
     ),
     'nikic/php-parser' => 
     array (
